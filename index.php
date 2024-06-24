4cf02a27a7cca3fbc19571c8d3e516727920d4d4
salvatorebriga <salvo.z2302@gmail.com>
2024-06-24 18:58:14 +0200

    Bonus 1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ class Movie
 {
     private string $titolo;
     private int $anno;
-    public array $genere;
+    private array $genere = [];
 
     public function __construct(string $_titolo)
     {
@@ -31,21 +31,26 @@ class Movie
         return $this->anno;
     }
 
-    public function setMovieGenere(int $_genere): void
+    public function setMovieGenere(array $_genere): void
     {
-        $this->anno = $_genere;
+        $this->genere = $_genere;
     }
 
     public function getMovieGenere(): array
     {
         return $this->genere;
     }
+
+    public function addMovieGenere(string $_genere): void
+    {
+        $this->genere[] = $_genere;
+    }
 }
 
 $fight_club = new Movie('Fight Club');
 $il_gladiatore = new Movie('Il Gladiatore');
 
-//Gestiscto due exception per settare l'anno di un Movie
+//Gestisco due exception per settare l'anno di un Movie
 try {
     //presupponiamo che questo valore arrivi tramite input utente
     $inputGiusto = 1999;
@@ -74,12 +79,17 @@ try {
     echo 'Eccezione: ' . $e->getMessage();
 }
 
+// metodo set
+$fight_club->setMovieGenere(['Drama', 'Action']);
+$il_gladiatore->setMovieGenere(['Historical', 'Adventure', 'Drama']);
+// metodo add, la differenza è chiara
+$fight_club->addMovieGenere('Psychological');
+$fight_club->addMovieGenere('Yellow');
+$il_gladiatore->addMovieGenere('Action');
+$il_gladiatore->addMovieGenere('Compelling');
 
 
-
-
-
-
+// stampa dei risultati
 var_dump($fight_club);
 echo '<br>';
 var_dump($il_gladiatore);
